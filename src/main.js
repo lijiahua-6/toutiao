6d1f51cc8f507cb4fe6a7974462124895141ac33
lijiahua <lijiahua@163.com>
2020-01-06 17:16:59 +0800

    项目初始化，封装注册vant组件模块

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
+import Vue from 'vue' // 引入vue
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import './utils/register-vant.js' // 加载执行注册模块
+// 注册需要的组件 先引入过来
 
 Vue.config.productionTip = false
 
---
+++ src/utils/register-vant.js
@@ -0,0 +1,15 @@
+import Vue from 'vue' // 引入vue
+
+// 加载按钮
+import {
+  Button,
+  Cell,
+  CellGroup
+} from 'vant'
+  // 注册按钮
+Vue
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)
+
+// 之后所有 Vant 组件的注册就都写到上面的模块中，而组件的使用该怎么使用还是怎么使用
